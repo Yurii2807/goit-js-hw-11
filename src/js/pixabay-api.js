ff2005584f3fb9070bb97a791da06b229e361052
Yurii2807 <156546118+Yurii2807@users.noreply.github.com>
2024-06-15 17:37:13 +0300

    update pixabay.api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 export const fetchImages = requestInput => {
   return fetch(
-    `https://pixabay.com/api/?key=44364811-49b1c0cdbd7356dfdac934983q=${encodeURIComponent(
+    `https://pixabay.com/api/?key=44364811-49b1c0cdbd7356dfdac934983&q=${encodeURIComponent(
     requestInput
     )}&image_type=photo&orientation=horizontal&safesearch=true`)
     .then(response => {
